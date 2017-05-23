cbefc80908a2117cfd63248df57a8197b726b1a4
Michał Jung <stackustack@gmail.com>
2017-05-23 15:43:15 +0200

    new template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="app">
 		<app-header></app-header>
-		<el-row>
-			<app-steps-list></app-steps-list>
+		<el-row :gutter="5">
+			<app-sidebar></app-sidebar>
 			<app-core></app-core>
 		</el-row>
 	</div>
@@ -11,13 +11,13 @@
 <script>
 	import appHeader from './components/Header.vue'
 	import appCore from './components/Core.vue'
-	import appStepsList from './components/StepsList.vue'
+	import appSidebar from './components/Sidebar.vue'
 
 	export default {
 		components: {
 			appHeader,
 			appCore,
-			appStepsList
+			appSidebar
 		}
 	}
 </script>
@@ -29,10 +29,16 @@
 
 body {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	background-color: #1F2D3D;
 }
 
 #app {
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
+}
+
+.grid-content {
+    border-radius: 2px;
+    background-color: #EFF2F7;
 }
 </style>
---
+++ src/components/Core.vue
@@ -0,0 +1,36 @@
+<template>
+	<el-col :span="20">
+        <div class="grid-content">
+            <el-form :inline="true" :model="formInline">
+                <el-form-item label="Reviewer">
+                    <el-input v-model="formInline.user" placeholder="Provide your name here"></el-input>
+                </el-form-item>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">Query</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+	</el-col>		
+</template>
+
+
+<script>
+	export default {
+        data() {
+            return {
+                formInline: {
+                    user: '',
+                    region: ''
+                }
+            }
+        }
+	}
+</script>
+
+<style scoped>
+    .el-form {
+        border: red solid 1px;
+        padding: 20px;
+    }
+</style>
