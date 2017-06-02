58de44c72cdc1e98c3aca264bb0e022eb8e0212d
Michał Jung <michal.jung@netguru.pl>
2017-06-02 19:43:03 +0200

    facepalm

---
+++ src/secrets.js
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+    apiKey:             "fill-me-with-firebase-stuff",
+    authDomain:         "fill-me-with-firebase-stuff",
+    databaseURL:        "fill-me-with-firebase-stuff",
+    projectId:          "fill-me-with-firebase-stuff",
+    storageBucket:      "fill-me-with-firebase-stuff",
+    messagingSenderId:  "fill-me-with-firebase-stuff"
+};
